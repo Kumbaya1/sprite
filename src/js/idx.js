8e61082975dff13f1f1e89ae1b055e47e9b76660
Kumbaya1 <790023580@qq.com>
2018-11-30 18:41:25 +0800

    豆子移动

--- src/js/idx.js
+++ src/js/idx.js
@@ -5,7 +5,7 @@ const path = require('path');
 window.addEventListener('touchstart', function (e) {
     e.preventDefault()
 }, {passive: false});
-// document.oncontextmenu=function(){return false;}
+document.oncontextmenu=function(){return false;}
 /*
 *   chance 可用次数
 *   victory 胜利次数
@@ -13,6 +13,7 @@ window.addEventListener('touchstart', function (e) {
 *   sizeGap 每次力度增加最小单位
 *   active 力度条状态
 *   timeGap 每次力度变化时的时间最小间隔
+*   beansActive touch时保存豆的初始状态
 */
 class BeansGame {
     constructor({
@@ -41,6 +42,10 @@ class BeansGame {
             this.width = this.layer.resolution[0];
             this.height = this.layer.resolution[1];
             this.size = this.width / 10;
+            this.beansActive = {pos:[]};
+            this.machine = {instance:null,pos:[]};
+            console.log(this.width)
+            console.log(this.height)
 
         } catch (e) {
             console.error(e)
@@ -48,37 +53,56 @@ class BeansGame {
     }
 
     init({left = 10, top = 10, gap = 10, rowNum = 2} = {}) {
+        //绘制豆子
         for (let i = 0; i < this.chance; i++) {
             let row = Math.ceil((i + 1) / rowNum);  //第几行
             let col = i % rowNum + 1;               //第几列
             let pLeft = left + this.size / 2 + (gap + this.size) * (col - 1);
             let pTop = top + this.size / 2 + (gap + this.size) * (row - 1);
-            console.log(pLeft, pTop);
+            // console.log(pLeft, pTop);
             let s = new this.Sprite(require('../img/i1.png')).attr({
                 anchor: 0.5,
                 size: [this.size, this.size],
                 pos: [pLeft, pTop],
             });
             this.layer.append(s)
-            console.log(s)
-            let _this = this;
-            s.on('touchstart',function (evt) {
-                console.log(evt)
-                console.log(this)
-                _this.sprittouchStart()
-            })
-
+            // console.log(s)
+            let _self = this;
+            s.on('touchstart',(evt)=>{_self.spriteTouchStart(s,evt,_self)})
         }
+        //绘制豆浆机
+
+
     }
-    sprittouchStart(evt,s){
+    spriteTouchStart(context,evt,_self){
+        // console.log('spriteTouchStart')
+        _self.beansActive.pos = [evt.layerX,evt.layerY];  // 存储开始拖拽时的位置
+        context.on('touchmove',(evt)=>{_self.spriteTouchMove(context,evt,_self)})
+    }
+    spriteTouchMove(context,evt,_self){
+        // console.log('spriteTouchMove')
         // console.log(evt)
-    }
-    sprittouchMove(){
+        context.attr({
+            pos:[evt.layerX,evt.layerY]
+        })
+        context.on('touchend',(evt)=>{_self.spriteTouchEnd(context,evt,_self)})
+        // context.on('touchend',_self.debounce(_self.spriteTouchEnd,3000))
 
     }
-    sprittouchEnd(){
+    spriteTouchEnd(context,evt,_self){
+        console.log('spriteTouchEnd')
 
     }
+    debounce(method,delay){
+        var timer=null;
+         return function(){
+              var context=this, args=arguments;
+              clearTimeout(timer);
+              timer=setTimeout(function(){
+                  method.apply(context,args);
+              },delay);
+          }
+  }
 }
 
 var Beans = new BeansGame({Scene: Scene, Sprite: Sprite});
